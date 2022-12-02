779e4ae2e4590e99b6eaf2a6a60472f6a0867d85
Michel GILBERT <michel.1.gilbert@arval.fr>
2022-12-02 13:41:52 +0100

    Add Cart, resume cart

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,15 +2,29 @@
 
 namespace App\Controller;
 
+use App\Classe\Cart;
+use App\Entity\Order;
+use App\Entity\OrderDetails;
 use App\Form\OrderType;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
     #[Route('/commande', name: 'app_order')]
-    public function index(): Response
+    public function index(Cart $cart, Request $request): Response
     {
         if (!$this->getUser()->getAddresses()->getValues()) {
             return $this->redirectToRoute('app_account_address_add');
@@ -21,6 +35,63 @@ class OrderController extends AbstractController
 
         return $this->render('order/index.html.twig', [
             'form' => $form->createView(),
+            'cart' => $cart->getFull()
         ]);
+    }
+
+    #[Route('/commande/recapitulatif', name: 'app_order_recap', methods: 'POST')]
+    public function add(Cart $cart, Request $request): Response
+    {
+
+        $form = $this->createForm(OrderType::class, null, [
+            'user' => $this->getUser(),
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $date = new \DateTimeImmutable();
+            $carriers = $form->get('carriers')->getData();
+            $delivery = $form->get('addresses')->getData();
+            $delivery_content = $delivery->getFirstname() . ' ' . $delivery->getLastname();
+            $delivery_content .= '<br>' . $delivery->getPhone();
+
+            if ($delivery->getCompany()) {
+                $delivery_content .= '<br>' . $delivery->getCompany();
+            }
+            $delivery_content .= '<br>' . $delivery->getAddress();
+            $delivery_content .= '<br>' . $delivery->getCity() . ' ' . $delivery->getCity();
+            $delivery_content .= '<br>' . $delivery->getCountry();
+
+            //enregistrer ma commande order()
+            $order = new Order();
+            $order->setUser($this->getUser());
+            $order->setcreatedAt($date);
+            $order->setCarrierName($carriers->getName());
+            $order->setCarrierPrice($carriers->getPrice());
+            $order->setDelivery($delivery_content);
+            $order->setisPaid(0);
+
+            $this->entityManager->persist($order);
+
+            //Enregistrer mes produits orderDetails()
+            foreach ($cart->getFull() as $product) {
+                $orderDetails = new OrderDetails();
+                $orderDetails->setMyOrder($order);
+                $orderDetails->setProduct($product['product']->getName());
+                $orderDetails->setQuantity($product['quantity']);
+                $orderDetails->setPrice($product['product']->getPrice());
+                $orderDetails->setTotal($product['product']->getPrice() * $product['quantity']);
+                $this->entityManager->persist($orderDetails);
+            }
+            //$this->entityManager->flush();
+
+            return $this->render('order/add.html.twig', [
+                'cart' => $cart->getFull(),
+                'carrier' => $carriers,
+                'delivery' => $delivery_content,
+            ]);
+        }
+        return $this->redirectToRoute('app_cart');
     }
 }
--- src/Entity/Carrier.php
+++ src/Entity/Carrier.php
@@ -63,4 +63,9 @@ class Carrier
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->getName() . '[br]' . $this->getDescription() . '[br]' . number_format($this->getPrice(), 2, ',', ' ') . ' €';
+    }
 }
